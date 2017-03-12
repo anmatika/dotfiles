11e4bba01ae12b64af2e0bf06f5f666fa5a40304
anmatika <4ntti.matikainen@gmail.com>
2017-03-12 10:03:19 +0200

    xbindkeys

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = exports = {
+  "env": {
+    "es6": true,
+    "node": true
+  },
+
+  "extends": "airbnb",
+  "rules": {
+    "indent": ["error", 4],
+    "comma-dangle": ["off"],
+    "import/no-extraneous-dependencies": ["off"],
+    "no-console": ["off"],
+    "semi": ["off"]
+  }
+}
