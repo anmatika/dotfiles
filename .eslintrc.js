85d74be4f89dec24360be350c64daba14f5e7e66
anttim <antti.matikainen@sievo.com>
2017-06-03 20:36:02 +0300

    dotfiles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,37 @@
-module.exports = exports = {
-  "env": {
-    "es6": true,
-    "node": true
-  },
-
-  "extends": "airbnb",
-  "rules": {
-    "indent": ["error", 2],
-    "comma-dangle": ["off"],
-    "import/no-extraneous-dependencies": ["off"],
-    "no-console": ["off"],
-    "semi": ["off"]
-  }
-}
+module.exports = {
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "settings": {
+        "import/resolver": {
+            "node": {
+                "paths": [
+                    __dirname
+                ]
+            }
+        }
+    },
+    "extends": "airbnb",
+    "rules": {
+        "indent": ["error", 4],
+        "react/jsx-indent": ["error", 4],
+        "no-unused-vars": ["warn", { "vars": "all", "args": "after-used" }],
+        "linebreak-style": ["off"],
+        "no-underscore-dangle": ["off"],
+        "comma-dangle": ["off"],
+        "quotes": ["off"],
+        "max-len": ["off"],
+        "no-plusplus": ["off"],
+        "no-use-before-define": ["off"],
+        "no-new": ["off"],
+        "react/jsx-uses-vars": ["error"],
+        "react/prefer-es6-class": ["off"],
+        "import/extensions": ["off"],
+        "quotes": ["error", "single"],
+        "import/no-unresolved": ["error", { caseSensitive: false }],
+        "no-confusing-arrow": ["off"],
+        "padded-blocks": ["off"]
+    }
+};
